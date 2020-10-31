3c6861c68a3f97697184b5e141526058c3245712
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2020-10-31 15:45:52 -0300

    Implementaçào do WriteCSV

--- Projeto1-POO/src/projeto1/poo/WriteCSV.java
+++ Projeto1-POO/src/projeto1/poo/WriteCSV.java
@@ -1,21 +1,40 @@
 package projeto1.poo;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.Map;
+import java.util.SortedMap;
 
 public class WriteCSV {
 
-    public WriteCSV(TreeMap<String, List<String>> adjMap, String fileName) {
-        
-    }  
-
-    private void writeFile() {
-
+    public WriteCSV(SortedMap<String, List<String>> adjMap, String fileName) {
+        writeFile(adjMap, fileName);
     }
 
-    private void orderWords(TreeMap<String,ArrayList<String>> adjMap) {
-        
+    private void writeFile(SortedMap<String, List<String>> adjMap, String fileName) {
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            
+            FileWriter fileWriter = new FileWriter(file);
+            for (Map.Entry<String, List<String>> entry : adjMap.entrySet()) {
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+                fileWriter.write(key);
+                for (String string : value) {
+                    fileWriter.write(", " + string);
+                }
+                fileWriter.write("\n");
+            }
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException iOException) {
+            
+        }
     }
 
 }
