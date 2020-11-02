f52391a99d8e93f62e31299580edc9ccae2a6b50
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2020-11-02 18:37:27 -0300

    JavaDoc, ainda em português

--- Projeto1-POO/src/projeto1/poo/Main.java
+++ Projeto1-POO/src/projeto1/poo/Main.java
@@ -4,19 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Responsável pela interface com o usuário, recebendo todos os arquivos
+ * desejados. Realiza a verificação pela extensão .txt. <br>
+ * A saída é feita com o mesmo nome de arquivo, apenas alterando a extensão para
+ * .csv
+ *
+ * @author Ariel Tadeu
+ * @author Enzo Fujimoto
+ * @author Guilherme Lopes
+ * @author Leonardo Teixeira
+ */
 public class Main {
 
+    /**
+     * Responável por iniciar todo o programa e passar o dados do usuário para
+     * o {@link Control}
+     */
     private void startProgram() {
         List<String> fileRequest = userTextInterface();
         Control control = new Control(fileRequest);
     }
     
+    /**
+     * Exibe ao usuário a interface de texto e requisita todos os caminhos
+     * completos para os arquivos que serão transformados.
+     * 
+     * @return Lista com todos os caminhos completos para serem processados
+     */
     private List<String> userTextInterface() {
         Scanner reader = new Scanner(System.in);
         List<String> fileRequest = new ArrayList();
         String lineString;
-        System.out.println("Write each file path you want. Then 0 to stop");
-        do {            
+        System.out.println("Write each file path you want (FULL PATH). Then 0 to stop");
+        do {
             lineString = reader.nextLine();
             if (!"0".equals(lineString)) {
                 fileRequest.add(lineString);
@@ -24,10 +45,15 @@ public class Main {
                 break;
             }
         } while (true);
-        
+
         return fileRequest;
     }
-    
+
+    /**
+     * <b>Entrada principal do programa</b>
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Main main = new Main();
         main.startProgram();
--- Projeto1-POO/src/projeto1/poo/WriteCSV.java
+++ Projeto1-POO/src/projeto1/poo/WriteCSV.java
@@ -8,22 +8,55 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 
+/**
+ * Responável por realizar a saída de um {@link Digraph Digraph} em um arquivo
+ * de texto em formato .csv
+ *
+ * @author Ariel Tadeu
+ * @author Enzo Fujimoto
+ * @author Guilherme Lopes
+ * @author Leonardo Teixeira
+ */
 public class WriteCSV {
 
+    /**
+     * Realiza a chamada do método caso contenha um {@code fileName} com 
+     * extensão .csv
+     * {@link WriteCSV#writeFile(java.util.SortedMap, java.lang.String) WriteCSV}
+     *
+     *
+     * @param adjMap Representação da lista de adjacência
+     * @param fileName Nome utilizado como arquivo de saida, necessáriamente com
+     * extensão .csv
+     */
     public WriteCSV(SortedMap<String, List<String>> adjMap, String fileName) {
-        writeFile(adjMap, fileName);
+        try {
+            if (!fileName.endsWith(".csv")) // Checks for .csv as file extension
+            {
+                throw new Exception(fileName + "isn`t a valid .csv file.");
+            }
+            writeFile(adjMap, fileName);
+        } catch (Exception ex) {
+            System.err.println("Invalid file!\n" + ex.getLocalizedMessage());
+        }
     }
 
+    /**
+     * Escreve todo o {@link adjMap} no arquivo {@link fileName} em formato .csv
+     *
+     * @param adjMap Representação da lista de adjacência
+     * @param fileName Nome utilizado como arquivo de saida, necessáriamente
+     */
     private void writeFile(SortedMap<String, List<String>> adjMap, String fileName) {
         try {
             File file = new File(fileName);
             if (!file.exists()) {
                 file.createNewFile();
             }
-            
+
             FileWriter fileWriter = new FileWriter(file, Charset.forName("UTF-8"));
             System.out.println("Writing file " + fileName);
-            
+
             writeEachLine(adjMap, fileWriter);
             fileWriter.flush();
             fileWriter.close();
@@ -32,6 +65,15 @@ public class WriteCSV {
         }
     }
 
+    /**
+     * Para cada chave em {@link adjMap} cria uma linha e escreve todo o conteúdo
+     * de {@link fileName}
+     * 
+     * @param adjMap Representação da lista de adjacência
+     * @param fileName Nome utilizado como arquivo de saida, necessáriamente
+     * 
+     * @throws IOException 
+     */
     private void writeEachLine(SortedMap<String, List<String>> adjMap, FileWriter fileWriter) throws IOException {
         for (Map.Entry<String, List<String>> entry : adjMap.entrySet()) {
             String key = entry.getKey();
